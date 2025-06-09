a4df28f7d1ffa9af9743977cf7d36a2d59893944
Miswani <Rayyan.Miswani@Sobeys.com>
2025-06-09 11:34:57 -0400

    Initial commit - Completed Application

---
+++ FetchUserData/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter PLU Number: ");
+        string pluNumber = Console.ReadLine();
+
+
+        string connectionString = "Server=ASB2012SV011LAB;Database=nexris;Trusted_Connection=True;";
+        string query = "SELECT * FROM item WHERE PLUNmbr = @PLUNmbr";
+
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@PLUNmbr", pluNumber);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    Console.WriteLine("\n--- Item Details ---\n");
+
+                    while (reader.Read())
+                    {
+                        // Display all columns
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            Console.WriteLine($"{reader.GetName(i)}: {reader[i]}");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No item found with that PLU Number.");
+                }
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        Console.WriteLine("\nPress any key to exit.");
+        Console.ReadKey();
+    }
+}
